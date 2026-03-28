6b3b9c243485d48f8c2a474e02c02c954351997c
Aman <yaman49751@gmail.com>
2026-03-28 22:15:37 +0530

    added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from 'next'
 import { Poppins, Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './globals.css'
 
 const poppins = Poppins({ 
@@ -43,6 +44,7 @@ export default function RootLayout({
       <body className={`${poppins.variable} ${inter.variable} font-sans antialiased`}>
         {children}
         <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   )
